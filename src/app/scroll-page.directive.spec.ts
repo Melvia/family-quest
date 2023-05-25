af0b1e0fa31f84705c78205495fc086cc0bfce0a
Nata <melvia.nr@gmail.com>
2023-05-25 10:48:39 +0500

    alien by directive static

--- src/app/scroll-page.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ScrollPageDirective } from './scroll-page.directive';
-
-describe('ScrollPageDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ScrollPageDirective();
-    expect(directive).toBeTruthy();
-  });
-});
